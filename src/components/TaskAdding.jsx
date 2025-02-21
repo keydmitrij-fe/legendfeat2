df9353f629e95666a17104532a8fff7f73c3e8b8
Alexey Klusovich <alex.klusovich@icloud.com>
2025-02-21 12:14:00 +0300

    add adding task

--- src/components/TaskAdding.jsx
+++ src/components/TaskAdding.jsx
@@ -1,25 +1,32 @@
+import { useState } from "react";
 import "./TaskAdding.css";
 
 
-const inputElement = document.getElementsByClassName("input").value;
+let inputElementValue = document.getElementsByClassName("input").value;
 export default function TaskAdding() {
-  async function addTask(title, isDone) {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleChange(value) {
+    setInputValue(() => value);
+  }
+  async function addTask() {
     await fetch("https://easydev.club/api/v2/todos", {
       method: "POST",
       "Content-type": "application/json",
       body: JSON.stringify({
-        title: title,
-        isDone: true,
+        title: inputValue,
+        isDone: false,
       }),
     });
   }
   return (
     <div>
       <form className="form">
-        <input type="text" className="input" placeholder="Task To Be Done..." />
+        <input type="text" className="input" placeholder="Task To Be Done..." onChange={(event) => handleChange(event.target.value)} />
         <button
+          type="submit"
           className="button"
-          onClick={() => addTask(inputElement)}
+          onClick={() => addTask(inputElementValue)}
         >
           Add
         </button>
