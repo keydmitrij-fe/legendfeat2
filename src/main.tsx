0fb8acddac1c49f867dd233936d3a1ee822802e9
keydmitrij-fe <241766034+keydmitrij-fe@users.noreply.github.com>
2026-03-22 12:41:06 +0300

    BREAKING CHANGE(deps): migrate from CRA to Vite

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/index";
-import "./index.css";
+import "./main.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   // <StrictMode>
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
   // </StrictMode>
 );
