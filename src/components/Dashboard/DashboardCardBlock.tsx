461dc780c218343c9a9285c1895a50e15c4efa1d
keydmitrij-fe <241766034+keydmitrij-fe@users.noreply.github.com>
2026-05-04 18:03:40 +0300

    Add styles to KPI cards

--- src/components/Dashboard/DashboardCardBlock.tsx
+++ src/components/Dashboard/DashboardCardBlock.tsx
@@ -7,14 +7,25 @@ interface Props {
 
 const DashboardCardBlock: React.FC<Props> = ({ metrics }) => {
   return (
-    <>
+    <div
+      style={{
+        border: "1px solid lightgrey",
+        borderRadius: 12,
+        padding: "1rem",
+        display: "flex",
+        gap: "1rem",
+      }}
+    >
       {metrics.map((metric) => (
-        <Card key={metric.id}>
+        <Card
+          style={{ height: "250px", width: "250px", textAlign: "center" }}
+          key={metric.id}
+        >
           <p>{metric.name}</p>
           <p>{metric.value}</p>
         </Card>
       ))}
-    </>
+    </div>
   );
 };
 export default DashboardCardBlock;
